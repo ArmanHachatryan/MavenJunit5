1637fcc6a1774fe63205f9e6bd0b1538b04f94c1
Arman Hachatryan <hacatran532@gmail.com>
2024-06-19 14:42:35 +0300

    String

--- auto/src/main/java/org/aston/exercise/Main.java
+++ auto/src/main/java/org/aston/exercise/Main.java
@@ -1,7 +1,17 @@
 package org.aston.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.aston.exercise.Strings.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Перевернуть строку
+        String text = "Hello world";
+
+        System.out.println(flip(text));
+        System.out.println(flip2(text));
+        System.out.println(flip3(text));
     }
 }
